e5acc726f25ef14a59164f4a567a87bc31307043
huhsway <seunghoe9542@naver.com>
2021-08-30 20:05:43 +0900

    Member 도메인 추가

---
+++ back/common/src/main/java/com/example/common/member/domain/Member.java
@@ -0,0 +1,45 @@
+package com.example.common.member.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    @Builder
+    public Member(String email, String password, String name, MemberRole role){
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+
+    public void encodingPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ back/common/src/main/java/com/example/common/member/domain/MemberRole.java
@@ -0,0 +1,5 @@
+package com.example.common.member.domain;
+
+public enum MemberRole {
+    ADMIN, USER
+}
