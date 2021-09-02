1e79b50e1a6d0804363faa1cd79c442578f6ded2
huhsway <seunghoe9542@naver.com>
2021-09-02 22:25:34 +0900

    message 컴포넌트, stores 추가

---
+++ front/src/stores/modules/messages.js
@@ -0,0 +1,54 @@
+const state = {
+  messageGroup: {
+    messageClass: '',
+    message: '',
+    timeoutEvent: null
+  }
+};
+
+const mutations = {
+	'ADD_MESSAGE' (state, {message, messageClass}) {
+		state.messageGroup = {
+			messageClass,
+			message
+		}
+
+		if (state.timeoutEvent) {
+			clearTimeout(state.timeoutEvent);
+		}
+		state.timeoutEvent = setTimeout(function() {
+			state.messageGroup = {
+				messageClass: '',
+				message: ''
+			}
+		}, 5000);
+	},
+	'CLEAR_MESSAGE' (state) {
+		state.messageGroup = {
+			messageClass: '',
+			message: ''
+		}
+	}
+}
+
+const actions = {
+  addMessage({commit}, obj) {
+    commit('ADD_MESSAGE', obj);
+  },
+  clearMessage({commit}) {
+    commit('CLEAR_MESSAGE');
+  }
+}
+
+const getters = {
+    messages: (state) => {
+    return state.messageGroup;
+  }
+};
+
+export default {
+	state,
+	mutations,
+	actions,
+	getters
+}
