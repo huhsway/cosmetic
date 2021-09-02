ee793fe68d8395e039cf92ab32e55811a11c47ed
huhsway <seunghoe9542@naver.com>
2021-09-02 22:23:18 +0900

    VueRouter, BootstrapVue, IconsPlugin 추가

--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { routes } from "./routes";
+import store from "./stores/store";
+import "./assets/styles/app.scss";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+const router = new VueRouter({
+  mode: "history",
+  routes
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  router,
+  store,
+  render: h => h(App)
+});
